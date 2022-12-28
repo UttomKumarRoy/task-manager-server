11cbd555e7754f0ee1811f11c3cd17822916e1b8
Uttom <uttom.teacher.gps2@gmail.com>
2022-12-28 23:51:29 +0600

    All API completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -18,22 +18,72 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const tasksCollection = client.db('taskManager').collection('tasks');
-        
 
+        app.get('/myTask',  async (req, res) => {
+            const email=req.query.email;  
+            const status=req.query.status;                    
+            const result = await tasksCollection.find({email, status}).toArray();
+            res.send(result);   
+        });
 
-
-        
+        app.get('/completedTask',  async (req, res) => {
+            const email=req.query.email;  
+            const status=req.query.status;                    
+            const result = await tasksCollection.find({email, status}).toArray();
+            res.send(result);   
+        });
 
         app.post('/addTask',  async (req, res) => {
             const task= req.body;
-           
             const result = await tasksCollection.insertOne(task);
-            res.send(result);
-            
-           
+            res.send(result); 
         });
 
-       
+        app.put('/myTask/:id', async (req,res) =>{
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: 'completed'
+                }
+            }
+            const result = await tasksCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+        app.put('/completedTask/:id', async (req,res) =>{
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    status: 'incomplete'
+                }
+            }
+            const result = await tasksCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+        app.put('/myTaskModal/:id', async (req,res) =>{
+            const id = req.params.id;
+            const task=req.body;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    taskTitle: task.taskTitle
+                }
+            }
+            const result = await tasksCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+
+        app.delete('/myTask/:id', async (req,res) =>{
+            const id=req.params.id;
+            const result= await tasksCollection.deleteOne({_id:ObjectId(id)});
+            res.send(result);
+        })
     
         
     }
